8ccd9ae581eac0b9eec87a70126caab890f4ae6b
KirillDyak0v <dyak0vkirill.dk@gmail.com>
2023-01-18 15:17:41 +0200

    Data Types

--- src/com/java/lessions/HelloWorld.java
+++ src/com/java/lessions/HelloWorld.java
@@ -3,7 +3,20 @@ package com.java.lessions;
 public class HelloWorld {
 
 	public static void main(String[] args) {
-		System.out.println("Hello World!");
+		int myInt = 18;
+		long myLong = 23245754;
+		short myShort = 256;
+		
+		double myDouble = 216.67;
+		float myFloat = 876.3f;
+		
+		char myChar = '+';
+		boolean myBoolean = true;
+		
+		byte myByte = 120;
+		
+		System.out.println("*****Data Types*****\n" + "\n"+ "*Int: " + myInt + "\n" + "**Long: " + myLong + "\n" + "***Short: " + myShort + "\n" + "****Double: " + myDouble + "\n" + "*****Float: " + myFloat + "\n" +
+		"******Char: " + myChar + "\n" + "*******Boolean: " + myBoolean + "\n" + "********Byte: " + myByte+"\n"+"\n"+"********************");
 
 	}
 

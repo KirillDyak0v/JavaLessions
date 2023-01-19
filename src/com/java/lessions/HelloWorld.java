035846dee14908250569dbe68b04142fa5a617fd
KirillDyak0v <dyak0vkirill.dk@gmail.com>
2023-01-19 14:38:44 +0200

    Reference Data Types

--- src/com/java/lessions/HelloWorld.java
+++ src/com/java/lessions/HelloWorld.java
@@ -3,21 +3,11 @@ package com.java.lessions;
 public class HelloWorld {
 
 	public static void main(String[] args) {
-		int myInt = 18;
-		long myLong = 23245754;
-		short myShort = 256;
-		
-		double myDouble = 216.67;
-		float myFloat = 876.3f;
-		
-		char myChar = '+';
-		boolean myBoolean = true;
-		
-		byte myByte = 120;
-		
-		System.out.println("*****Data Types*****\n" + "\n"+ "*Int: " + myInt + "\n" + "**Long: " + myLong + "\n" + "***Short: " + myShort + "\n" + "****Double: " + myDouble + "\n" + "*****Float: " + myFloat + "\n" +
-		"******Char: " + myChar + "\n" + "*******Boolean: " + myBoolean + "\n" + "********Byte: " + myByte+"\n"+"\n"+"********************");
-
+		int age = 18;
+		String name = "Kirill";
+		String space = " ";
+		System.out.println(name + space + age);
 	}
 
 }
+  

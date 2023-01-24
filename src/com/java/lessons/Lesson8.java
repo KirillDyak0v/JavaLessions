90ef54ea86d837636a3c9706b81a69cbc2da9ba8
KirillDyak0v <dyak0vkirill.dk@gmail.com>
2023-01-24 14:44:19 +0200

    Lesson 8

---
+++ src/com/java/lessons/Lesson8.java
@@ -0,0 +1,17 @@
+package com.java.lessons;
+
+import java.util.Scanner;
+
+public class Lesson8 {
+
+	public static void main(String[] args) {
+	Scanner sc = new Scanner(System.in); 
+	int value;
+	do{
+		System.out.println("Введи число 5");
+		 value = sc.nextInt();
+		}while(value!=5);
+		System.out.println("Вы ввели 5");
+	}
+
+}

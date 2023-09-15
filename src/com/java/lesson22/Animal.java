877adfe4bfbf63a9b9c2453aadf61369c70682a7
KirillDyak0v <dyak0vkirill.dk@gmail.com>
2023-09-15 18:20:30 +0300

    Lesson22|Наследование

---
+++ src/com/java/lesson22/Animal.java
@@ -0,0 +1,9 @@
+package com.java.lesson22;
+
+public class Animal {
+    public void eat(){
+        System.out.println("Im eating");
+    }public void sleep(){
+        System.out.println("Im Sleeping");
+    }
+}
---
+++ src/com/java/lesson22/Dog.java
@@ -0,0 +1,8 @@
+package com.java.lesson22;
+
+public class Dog extends Animal {
+    public void bark(){
+        System.out.println("Im barking");
+    }
+    
+}

bc57cdd97bd7eaf6da32738e29e96ee449d0e029
unknown <dyak0vkirill.dk@gmail.com>
2023-11-23 22:45:06 -1200

    Lesson23|Полиморфизм

--- src/com/java/lesson23/Test.java
+++ src/com/java/lesson23/Test.java
@@ -4,10 +4,9 @@ public class Test {
     public static void main(String[] args){
         Animal animal = new Animal();
         Dog dog = new Dog();
-        Cat cat = new Cat();
         test(animal);
         test(dog);
-        test(cat);
+
     }
     public static void test(Animal animal){
         animal.eat();
